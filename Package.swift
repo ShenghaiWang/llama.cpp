fa52e0f49a6220d8d2ef271762cfce067d3a09d9
Tim Wang <tim.wang@ing.com>
2025-02-22 10:47:57 +1100

    Update package

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
         .library(name: "llama", targets: ["llama"]),
     ],
     targets: [
-        .systemLibrary(name: "llama", pkgConfig: "llama"),
+        .systemLibrary(name: "llama"),
     ]
 )
